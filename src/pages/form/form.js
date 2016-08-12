6832c8424fd285cfc480f27fa4fab55f1c175c08
Marcio Gasparotto <mgasparotto@gmail.com>
2016-08-12 18:34:13 -0300

    wip

--- src/pages/form/form.js
+++ src/pages/form/form.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {PrismCode} from 'react-prism';
+import ShowCode from '../../components/ShowCode.jsx';
 
 const FormPage = (props) => {
 
@@ -33,7 +34,114 @@ const FormPage = (props) => {
           </label>
         </form>
 
+        <ShowCode>
+          <PrismCode className='language-html'>
+            {require('!raw-loader!./simpleForm.html')}
+          </PrismCode>
+        </ShowCode>
+
         <div className='sv-vertical-marged-50' />
+
+
+
+
+        <h5 className='sv-title'>Form with full width</h5>
+
+        <form className='sv-form--full'>
+          <label>
+            <span>Name</span>
+            <input type='text' name='name' />
+          </label>
+          <label>
+            <span>E-mail</span>
+            <input type='text' name='email' />
+          </label>
+          <label>
+            <span>Disabled Field</span>
+            <input type='text' name='login' disabled value='Disabled field' />
+          </label>
+          <label>
+            <span>Gender</span>
+            <select>
+              <option value=''>Please, select</option>
+              <option value='M'>Male</option>
+              <option value='F'>Female</option>
+            </select>
+          </label>
+        </form>
+
+        <ShowCode>
+          <PrismCode className='language-html'>
+            {require('!raw-loader!./simpleFormFull.html')}
+          </PrismCode>
+        </ShowCode>
+
+        <div className='sv-vertical-marged-50' />
+
+
+
+        <h5 className='sv-title'>Form with full width and grid</h5>
+
+        <form className='sv-form--full'>
+
+          <div className='sv-container'>
+            <div className='sv-grid-6 sv-grid-12-xs'>
+              <label>
+                <span>Name</span>
+                <input type='text' name='name' />
+              </label>
+            </div>
+            <div className='sv-grid-6 sv-grid-12-xs'>
+              <label>
+                <span>E-mail</span>
+                <input type='text' name='email' />
+              </label>
+            </div>
+            <div className='sv-grid-4 sv-grid-6-md sv-grid-12-xs'>
+              <label>
+                <span>Gender</span>
+                <select>
+                  <option value=''>Please, select</option>
+                  <option value='M'>Male</option>
+                  <option value='F'>Female</option>
+                </select>
+              </label>
+            </div>
+            <div className='sv-grid-4 sv-grid-6-md sv-grid-12-xs'>
+              <label>
+                <span>Password</span>
+                <input type='text' name='password' />
+              </label>
+            </div>
+            <div className='sv-grid-4 sv-grid-6-md sv-grid-12-xs'>
+              <label>
+                <span>Confirm Password</span>
+                <input type='text' name='password' />
+              </label>
+            </div>
+
+            <div className='sv-grid-6'>
+              <label className='sv-label--group'>
+                <span>Conditions:</span>
+              </label>
+              <label className='sv-label--checkbox'><input className='sv-checkbox' type='checkbox' />Send Mails</label>
+              <label className='sv-label--checkbox'><input className='sv-checkbox' type='checkbox' />I Accept</label>
+            </div>
+
+            <div className='sv-grid-6'>
+              <label className='sv-label--group'>
+                <span>Gender:</span>
+              </label>
+              <label className='sv-label--radio'><input className='sv-radio' type='radio' name="gender" />Male</label>
+              <label className='sv-label--radio'><input className='sv-radio' type='radio' name="gender" />Female</label>
+            </div>
+          </div>
+
+
+        </form>
+
+        <div className='sv-vertical-marged-50' />
+
 
         <h5 className='sv-title'>Inline Form</h5>
         <form className='sv-form--inline'>
@@ -56,6 +164,12 @@ const FormPage = (props) => {
           <button className='sv-button default' type='button'>Send</button>
         </form>
 
+        <ShowCode>
+          <PrismCode className='language-html'>
+            {require('!raw-loader!./inlineForm.html')}
+          </PrismCode>
+        </ShowCode>
+
         <div className='sv-vertical-marged-50' />
 
         <h5 className='sv-title'>Status</h5>
@@ -70,6 +184,14 @@ const FormPage = (props) => {
           </label>
         </form>
 
+        <ShowCode>
+          <PrismCode className='language-html'>
+            {require('!raw-loader!./inputStatus.html')}
+          </PrismCode>
+        </ShowCode>
+
+        <div className='sv-vertical-marged-50' />
+
         <h5 className='sv-title'>Input Group</h5>
         <form className='sv-form'>
           <div className='sv-input-group'>
@@ -77,6 +199,12 @@ const FormPage = (props) => {
             <button className='sv-button primary' type='button'>Enviar</button>
           </div>
         </form>
+
+        <ShowCode>
+          <PrismCode className='language-html'>
+            {require('!raw-loader!./inputGroup.html')}
+          </PrismCode>
+        </ShowCode>
       </div>
     </div>
   )
