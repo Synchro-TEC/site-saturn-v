ca10f978cafd4993a34bb9a230a3b7da2eb5f7b3
Marcio Gasparotto <mgasparotto@gmail.com>
2016-09-15 08:45:08 -0300

    wip

--- src/pages/Template.js
+++ src/pages/Template.js
@@ -4,38 +4,39 @@ import {Link} from 'react-router';
 function Template(props) {
   return (
     <div>
-      <input className='dm-sidebar-trigger-controller' id='show-menu' type='checkbox'  />
-      <label className='dm-sidebar-trigger' htmlFor='show-menu' />
-      <aside className='dm-sidebar'>
-        <header className='sv-text-center'>
-          <Link title='Back to home'  to='/'><img height='auto' src='sv-logo.svg' width='60%'/></Link>
-        </header>
-        <ul>
-          <li><Link activeClassName='active' to='install'>Install</Link></li>
-          <li><Link activeClassName='active' to='grid'>Grid</Link></li>
-          <li><Link activeClassName='active' to='tables'>Tables</Link></li>
-          <li><Link activeClassName='active' to='cards'>Cards</Link></li>
-          <li><Link activeClassName='active' to='forms'>Forms</Link></li>
-          <li><Link activeClassName='active' to='buttons'>Buttons</Link></li>
-          <li><Link activeClassName='active' to='typograph'>Typograph</Link></li>
-          <li><Link activeClassName='active' to='colors'>Colors</Link></li>
-          <li><Link activeClassName='active' to='helpers'>Helpers</Link></li>
-          <li><Link activeClassName='active' to='layouts'>Layouts</Link></li>
-          <li><Link activeClassName='active' to='components'>Components</Link></li>
-        </ul>
-        <div className='dm-syntec'>
-          <a href='https://github.com/Synchro-TEC' rel='noopen' target='_blank'>
-            <img height='auto' src='logo-synchro.png' width='100'/>
-          </a>
-          <br />
-          <a href='https://github.com/Synchro-TEC' rel='noopen' target='_blank'>
-            <img height='40' src='syntec.svg' width='auto'/>
-          </a>
-        </div>
-      </aside>
-      <section className='dm-main'>
+      <header className='dm-header'>
+        <Link title='Back to home'  to='/'><img height='auto' src='logo-text.svg' width='134px'/></Link>
+      </header>
+
+
+      {/*<aside className='dm-sidebar'>*/}
+
+        {/*<ul>*/}
+          {/*<li><Link activeClassName='active' to='install'>Install</Link></li>*/}
+          {/*<li><Link activeClassName='active' to='grid'>Grid</Link></li>*/}
+          {/*<li><Link activeClassName='active' to='tables'>Tables</Link></li>*/}
+          {/*<li><Link activeClassName='active' to='cards'>Cards</Link></li>*/}
+          {/*<li><Link activeClassName='active' to='forms'>Forms</Link></li>*/}
+          {/*<li><Link activeClassName='active' to='buttons'>Buttons</Link></li>*/}
+          {/*<li><Link activeClassName='active' to='typograph'>Typograph</Link></li>*/}
+          {/*<li><Link activeClassName='active' to='colors'>Colors</Link></li>*/}
+          {/*<li><Link activeClassName='active' to='helpers'>Helpers</Link></li>*/}
+          {/*<li><Link activeClassName='active' to='layouts'>Layouts</Link></li>*/}
+          {/*<li><Link activeClassName='active' to='components'>Components</Link></li>*/}
+        {/*</ul>*/}
+        {/*<div className='dm-syntec'>*/}
+          {/*<a href='https://github.com/Synchro-TEC' rel='noopen' target='_blank'>*/}
+            {/*<img height='auto' src='logo-synchro.png' width='100'/>*/}
+          {/*</a>*/}
+          {/*<br />*/}
+          {/*<a href='https://github.com/Synchro-TEC' rel='noopen' target='_blank'>*/}
+            {/*<img height='40' src='syntec.svg' width='auto'/>*/}
+          {/*</a>*/}
+        {/*</div>*/}
+      {/*</aside>*/}
+      {/*<section className='dm-main'>*/}
         {props.children}
-      </section>
+      {/* </section> */}
     </div>
   );
 }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,39 +2,8 @@ import React from 'react';
 
 const HomePage = () => (
   <div>
-    <p className='sv-text-center'>
-      <img src='sv-logo-rocket.svg' style={{width: 'auto', height: '60vh'}}/>
-    </p>
-    <div className='dm-about'>
-      <h1 className='sv-color-blue-grey-800'>Sobre</h1>
-      <p>
-        O Saturn-V nasceu da nossa necessidade por um Framework CSS que fosse leve, simples e com o foco apenas no desenvolvimento de
-        Web Applications.
-      </p>
-
-      <p>
-        Tentamos outros frameworks, mas a grande maioria fazem muito mais do que precisamos e além do visual tendencioso entregam
-        também componentes JavaScript e com isso, comportamento que nem sempre eram o que precisávamos.
-      </p>
-
-      <p>
-        No Saturn-V optamos pela simplicidade, nosso foco está em nossos produtos (Web Applications) e implementamos apenas o que
-        necessitamos, entregando somente o CSS.
-      </p>
-
-      <p>
-        Optamos por desenvolver separadamente os componentes JS, usando o Saturn-V como base, desta forma, podemos ter componentes
-        usando o Saturn-V com React, Angular, Vanilla, etc. O que é fundamental em nosso cenário.
-      </p>
-
-      <div className='sv-vertical-marged-50'></div>
-
-      <h3 className='sv-color-blue-grey-800'>Arquitetura</h3>
-      <p>
-        Optamos por usar uma arquitetura híbrida, usando diversos conceitos e patterns CSS como: <a
-        href='https://smacss.com/'>SMACSS</a>,&nbsp;
-        <a href='http://getbem.com/introduction/'>BEM</a> e <a href='http://itcss.io/'>ITCSS</a>.
-      </p>
+    <div className='col dm-hero'>
+a
     </div>
   </div>
 );
