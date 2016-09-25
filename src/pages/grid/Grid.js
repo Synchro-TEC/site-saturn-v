64d521de540000e019fa4c6086a818b291c1be35
Marcio Gasparotto <mgasparotto@gmail.com>
2016-09-25 20:43:36 -0300

    grid

--- src/pages/grid/Grid.js
+++ src/pages/grid/Grid.js
@@ -84,13 +84,11 @@ const GridPage = (props) => {
           </PrismCode>
         </ShowCode>
 
-        <h5>Grid Tricks</h5>
+        <h5>Grid with diferent column sizes</h5>
 
-        <p>Saturn V grid has few tricks to help you adjust columns sizes, you can use some values that represent a
-          percentage.<br />
-
-          Implemented values: 20, 25, 40, 60, 75 and 80 percent; To use it, just add a class with _ + value, ex: _20 to
-          create a column with 20%.</p>
+        <p>Saturn V grid has few tricks to help you adjust columns sizes, you can use some values that represent a percentage.<br />
+          Implemented values: 20, 25, 40, 60, 75 and 80 percent; To use it, just add a class with _ + value, ex: _20 to create a column with
+          20%.</p>
 
         <p>You can use <code>.sv-row--with-gutter</code> or <code>.sv-row</code>.</p>
 
@@ -118,7 +116,7 @@ const GridPage = (props) => {
 
         <ShowCode>
           <PrismCode className='language-html'>
-            {require('!raw-loader!./gridTricks.html')}
+            {require('!raw-loader!./gridDifferentColumnSize.html')}
           </PrismCode>
         </ShowCode>
 
@@ -126,7 +124,7 @@ const GridPage = (props) => {
         <h5>Fixed Columns</h5>
 
         <p>Saturn V grid has few fixed columns sizes, implemented fixed values: 100, 150, 200, 250 and 300 pixels.
-        <br /> To use it, just add a class with _ + value + --fixed, ex: _100--fixed create a column with fixed 100px.</p>
+          <br /> To use it, just add a class with _ + value + --fixed, ex: _100--fixed create a column with fixed 100px.</p>
 
         <div className='sv-row'>
           <div className='sv-column sv-bg-color--blue-50 _100--fixed'>100px</div>
@@ -160,6 +158,34 @@ const GridPage = (props) => {
         <ShowCode>
           <PrismCode className='language-html'>
             {require('!raw-loader!./gridFixed.html')}
+          </PrismCode>
+        </ShowCode>
+
+        <h5>Grid Tricks</h5>
+        <p>
+          If you need a <code>.sv-row</code> or <code>.sv-row--with-gutter</code> withou a bottom margin, add class
+          <code>.sv-no-margins</code>.
+        </p>
+        <p>See Helpers section to know more tricks.</p>
+
+        <div className='sv-row sv-no-margins'>
+          <div className='sv-column sv-bg-color--blue-50'>Column</div>
+        </div>
+
+        <div className='sv-row sv-no-margins'>
+          <div className='sv-column sv-bg-color--blue-100'>Column</div>
+          <div className='sv-column sv-bg-color--blue-200'>Column</div>
+        </div>
+
+        <div className='sv-row sv-no-margins'>
+          <div className='sv-column sv-bg-color--blue-200'>Column</div>
+          <div className='sv-column sv-bg-color--blue-300'>Column</div>
+          <div className='sv-column sv-bg-color--blue-400'>Column</div>
+        </div>
+
+        <ShowCode>
+          <PrismCode className='language-html'>
+            {require('!raw-loader!./gridTricks.html')}
           </PrismCode>
         </ShowCode>
       </div>
