180af51751cf282571eb4ad26ce50429afd1a5ee
Marcio Gasparotto <mgasparotto@gmail.com>
2016-09-29 00:07:17 -0300

    layouts

--- src/pages/layouts/LayoutsPage.js
+++ src/pages/layouts/LayoutsPage.js
@@ -7,6 +7,42 @@ const LayoutPage = (props) => {
     <div className='dm-content'>
       <h4 className='sv-title'>Layout</h4>
 
+      <div className='sv-row'>
+        <div className='sv-column'>
+          <div className='browser-mockup'>
+            <img height='auto' src='layout-01.svg' width='100%' />
+          </div>
+        </div>
+        <div className='sv-column'>
+          <div className='browser-mockup'>
+            <img height='auto' src='layout-02.svg' width='100%' />
+          </div>
+        </div>
+        <div className='sv-column'>
+          <div className='browser-mockup'>
+            <img height='auto' src='layout-03.svg' width='100%' />
+          </div>
+        </div>
+      </div>
+
+      <div className='sv-row'>
+        <div className='sv-column'>
+          <div className='browser-mockup'>
+            <img height='auto' src='layout-04.svg' width='100%' />
+          </div>
+        </div>
+        <div className='sv-column'>
+          <div className='browser-mockup'>
+            <img height='auto' src='layout-05.svg' width='100%' />
+          </div>
+        </div>
+        <div className='sv-column'>
+          <div className='browser-mockup'>
+            <img height='auto' src='layout-06.svg' width='100%' />
+          </div>
+        </div>
+      </div>
+
 
     </div>
   )
